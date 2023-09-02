9693370dea27d3911b18b439afa135a85ff9ad96
Emma <MarmadileManteater@proton.me>
2023-09-01 22:16:57 -0400

    Fix pretty-feed.js
    (was outputting to temp file 🤦‍♀️)

--- pretty-feed.js
+++ pretty-feed.js
@@ -5,4 +5,4 @@ const fs = require("fs")
 const feed = fs.readFileSync("feed.xml").toString()
 const resultFeed = prettifyXml(feed, { indent: 2, newline: '\n' })
 
-fs.writeFileSync("feed.xml.new", resultFeed)
+fs.writeFileSync("feed.xml", resultFeed)
